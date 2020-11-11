3e586aee15666ff5642a04d05b098ab422a988b6
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2020-11-11 07:22:32 +0000

    practising JS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const btnFetch = document.querySelectorAll(".btnFetch");
 const bookResults = document.querySelector(".container-images");
 const btnModalImages = document.querySelectorAll("button");
 let btns = document.getElementsByClassName("btnImageModal");
+let shoppingCartBooks = document.querySelector(".cart-items");
 
 //       console.log(parsedJson);
 //     })
@@ -21,7 +22,7 @@ const createBook = (book) => {
                              <img class="card-img-top d-flex" src="${book.img}">
                              <div class="card-body">
                                 <p class="card-text">
-                                <h6>"${book.title}"</h6> $${book.price}
+                                <h6 class="book-title">${book.title}</h6><span class = "book-price">$${book.price}</span>
                                 <div>
                                 <small class="text-muted">${book.category}</small>
                                 </div>
@@ -32,7 +33,7 @@ const createBook = (book) => {
                                         <button type="button" class="btn btn-sm btn-outline-secondary btnHide" onclick="hideToggle()">
                                         Skip
                                         </button>
-                                        <a><button type="button" class="btn btn-sm btn-outline-success" onclick="">
+                                        <a><button type="button" class="btn btn-sm btn-outline-success" onclick="addToCart(event)">
                                         Add to Cart<span><i class="fas fa-cart-plus ml-3"></i></span>
                                         </button></a>
                                     </div>
@@ -49,6 +50,7 @@ const getBooks = () => {
   })
     .then((response) => response.json())
     .then((catalogue) => {
+      console.log(catalogue)
       bookLibrary = catalogue;
       catalogue.forEach((item) => createBook(item));
       // bookResults
@@ -68,17 +70,35 @@ const hideToggle = () => {
 };
 
 const btn = document.querySelector(".search-btn");
-btn.addEventListener("click", function (e) {
-  e.preventDefault();
+btn.addEventListener("click", function (event) {
+  event.preventDefault();
   const input = document.querySelector(".form-control").value;
-  console.log(input)
-  bookResults.querySelectorAll("*").forEach((element) => element.remove());
+  // console.log(input)
+  // bookResults.querySelectorAll("*").forEach((element) => element.remove());
+  bookResults.innerHTML="" //same as above
   const filteredLibrary = bookLibrary.filter(item => item.title.toLowerCase().includes(input.toLowerCase())
   );
-  console.log(filteredLibrary)
-  console.log(bookLibrary)
+  // console.log(filteredLibrary)
+  // console.log(bookLibrary)
   filteredLibrary.forEach((book) => createBook(book));
 });
+
+
+const filterData = () => {
+let result = bookLibrary.filter(item => item.price<10 && item.category==='scifi')
+console.log(result)
+
+}
+//
+
+const findBook = (bookTitle) => {
+return bookLibrary.find(item => item.title === bookTitle)
+}
+
+const priceInCart = () => {
+  item.reduce((accumulator, current) => accumulator+item.price,0)
+}
+
 
 //   const basket = [];
 //   const addToCart = (asin) => basket.push(asin);
@@ -93,9 +113,58 @@ for (i = 0; i < closebtns.length; i++) {
   });
 }
 
+
+
+
+
+const addToCart = (e) => {
+let bookTitle = e.target.closest('.card-body').children[1].innerText
+const bookResult = findBook(bookTitle)
+let bookPrice = e.target.closest('.card-body').children[2].innerText
+const li = document.createElement("li");
+li.innerHTML = ` <li>${bookTitle}  ${bookPrice}<span class="close">&times;</span></li>`;
+shoppingCartBooks.appendChild(li);
+  };
+
+
+const mapping =() => {
+  const cont = document.querySelector('.second-container')
+  const mapped =bookLibrary.map(book=> ` <div class="col-sm-3 card mb-4 shadow-sm" >
+  <img class="card-img-top d-flex" src="${book.img}">
+  <div class="card-body">
+     <p class="card-text">
+     <h6 class="book-title">${book.title}</h6><span class = "book-price">$${book.price}</span>
+     <div>
+     <small class="text-muted">${book.category}</small>
+     </div>
+
+     </p>
+    <div class="justify-content-between align-items-center">
+         <div class="btn-group">
+             <button type="button" class="btn btn-sm btn-outline-secondary btnHide" onclick="hideToggle()">
+             Skip
+             </button>
+             <a><button type="button" class="btn btn-sm btn-outline-success" onclick="addToCart(event)">
+             Add to Cart<span><i class="fas fa-cart-plus ml-3"></i></span>
+             </button></a>
+         </div>
+         
+     </div>
+    </div>
+</div>` )
+cont.innerHTML=mapped
+  console.log(mapped)
+}
+
+
+
 window.addEventListener("DOMContentLoaded", (event) => {
   getBooks();
+
+
 });
+
+
 
 //   ${addToCart(
 //     book.asin
